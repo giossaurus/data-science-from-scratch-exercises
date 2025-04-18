c618788c0617864ddf1a8ad6e922768dc8266dc6
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2025-04-17 21:01:53 -0300

    Initialize finding key connectors data structure with sample users

---
+++ chapters/chapter one/findingKeyConnectors.py
@@ -0,0 +1,15 @@
+#Identificar os conectores chave dentre os cientistas de dados utilizando um dump de dad
+
+users = [
+    { "id" : 0, "name" : "Hero"},
+    { "id" : 1, "name" : "Dunn"},
+    { "id" : 2, "name" : "Sue"},
+    { "id" : 3, "name" : "Chi"},
+    { "id" : 4, "name" : "Thor"},
+    { "id" : 5, "name" : "Clive"},
+    { "id" : 6, "name" : "Hicks"},
+    { "id" : 7, "name" : "Devin"},
+    { "id" : 8, "name" : "Kate"},
+    { "id" : 9, "name" : "Klein"}
+]
+
