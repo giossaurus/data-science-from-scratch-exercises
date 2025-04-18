2cd06c068ba0f519af5968b493bcf83acd78efd6
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2025-04-17 21:21:17 -0300

    Add connection metrics calculation

---
+++ chapters/chapter_one/findingKeyConnectors.py
@@ -0,0 +1,50 @@
+#Identificar os conectores chave dentre os cientistas de dados utilizando um dump de dad
+
+users = [
+    { "id" : 0, "name" : "Hero"},
+    { "id" : 1, "name" : "Dunn"},
+    { "id" : 2, "name" : "Sue"},
+    { "id" : 3, "name" : "Chi"},
+    { "id" : 4, "name" : "Thor"},
+    { "id" : 5, "name" : "Clive"},
+    { "id" : 6, "name" : "Hicks"},
+    { "id" : 7, "name" : "Devin"},
+    { "id" : 8, "name" : "Kate"},
+    { "id" : 9, "name" : "Klein"}
+]
+
+friendship_pairs = [ 
+    (0, 1),
+    (0, 2),
+    (1, 2),
+    (1, 3),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6),
+    (5, 7),
+    (6, 8),
+    (7, 8),
+    (8,9)
+]
+
+
+friendships = {user["id"]: [] for user in users}
+
+for i, j in friendship_pairs:
+    friendships[i].append(j) # Adiciona o amigo j à lista de amigos do usuário i
+    friendships[j].append(i) # Adiciona o amigo i à lista de amigos do usuário j
+
+def number_of_friends(user):
+    """Quantos amigos o _user_ tem?"""
+    user_id = user["id"]
+    friend_ids = friendships[user_id]
+    return len(friend_ids)
+total_connections = sum(number_of_friends(user) for user in users)
+
+num_users = len(users)
+avg_connections = total_connections / num_users
+
+print(f"Total de conexões: {total_connections}")
+print(f"Total de usuários: {num_users}")
+print(f"Média de conexões: {avg_connections}")
